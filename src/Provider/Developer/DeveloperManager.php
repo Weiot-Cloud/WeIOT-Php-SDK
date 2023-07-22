22d44d5c14b2301af7ddcdf0c775d40ebac29d01
Cengiz AKCAN <thevobos@gmail.com>
2023-07-22 14:06:18 +0300

    Add files via upload

--- src/Provider/Developer/DeveloperManager.php
+++ src/Provider/Developer/DeveloperManager.php
@@ -39,4 +39,31 @@ class DeveloperManager extends WeIOTException{
 
     }
 
+
+    /**
+     * @param ConfigInterface $Config
+     * @return mixed
+     * @throws WeIOTException
+     * @throws WeIOTException|GuzzleException
+     */
+    public static function adminLogin(ConfigInterface $Config, $adminLoginAppToken) : mixed {
+
+        $client     = new Client(["base_uri" => $Config->apiServer]);
+        $request    = new Request('POST', sprintf("api/v1/admin/verify?initial%s",$adminLoginAppToken));
+
+        $response = $client->send($request, [
+            'form_params' => [
+                'id'    => $Config->devID,
+                'app'   => $Config->appID,
+                'token' => $Config->devToken,
+                'key'   => $Config->devKey,
+            ]
+        ]);
+
+        $responseCheck = json_decode($response->getBody());
+
+        return $responseCheck;
+
+    }
+
 }
